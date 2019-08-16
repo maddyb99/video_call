2725dc1d2780dedb6ff86a3920772f895d2a7f64
maddyb99 <maddyb99@users.noreply.github.com>
2019-08-16 18:50:40 +0530

    working normal login

---
+++ lib/Authentication/signUpReq.dart
@@ -0,0 +1,182 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import './../Utils/userData.dart';
+
+class Authenticate {
+  CollectionReference userReference;
+  QuerySnapshot userSnapshot;
+  AuthException exception;
+  Map<String, dynamic> details;
+  Future<void> Function(BuildContext context) fn;
+  String msg;
+  String verificationID;
+
+  Authenticate() {
+    //_isSignIn = false;
+    details = new Map<String, dynamic>();
+    getUser();
+    msg = "Invalid details";
+  }
+
+  void clear() {
+    details = new Map<String, dynamic>();
+    getUser();
+    msg = "Invalid details";
+    UserData.profileData = null;
+  }
+
+  void nextPage(BuildContext context) {
+    Navigator.pushNamedAndRemoveUntil(
+        context, '/home', ModalRoute.withName(':'));
+  }
+
+  Future<bool> isSignIn(context) async {
+    UserData.profileData = await FirebaseAuth.instance.currentUser();
+    try {
+      if (UserData.profileData == null) {
+        return false;
+      } else {
+        userSnapshot = await Firestore.instance
+            .collection('Users')
+            .where('emailID', isEqualTo: UserData.profileData.email)
+            .getDocuments();
+        UserData.detailsData = userSnapshot.documents[0];
+        nextPage(context);
+        return true;
+      }
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<void> _ackAlert(BuildContext context, String title, String content) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(content),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  getUser() async {
+    userReference = Firestore.instance.collection("Users");
+  }
+
+  getName(String x) => details.addAll({'Name': x});
+
+  getPass(String x) => details.addAll({'Pass': x});
+
+  getMobile(String x) => details.addAll({'Mobile': x});
+
+  getCountryCode(String x) => details.addAll({'CountryCode': x});
+
+  getEmail(String x) => details.addAll({'Email': x});
+
+  Future<void> signUp(
+      GlobalKey<FormState> _formKey, BuildContext context) async {
+    FormState formState = _formKey.currentState;
+    details.clear();
+    if (formState.validate()) {
+      formState.save();
+      try {
+        //_isSignIn = true;
+        print(details);
+        QuerySnapshot val = await userReference
+            .where("Mobile", isEqualTo: int.parse(details['Mobile']))
+            .getDocuments();
+        if (val.documents.length != 0) throw ("Mobile Number already in use");
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+            email: details['Email'], password: details['Pass']);
+        UserData.profileData = await FirebaseAuth.instance.currentUser();
+        UserData.profileData.sendEmailVerification();
+        currentUser = new Users(
+          email: details['Email'],
+          name: details['Name'],
+          m: details['Mobile'],
+        );
+        Firestore.instance.runTransaction((Transaction t) async {
+          await userReference
+              .document(UserData.profileData.uid)
+              .setData(currentUser.toJson());
+        });
+        userSnapshot = await userReference
+            .where('emailID', isEqualTo: details['Email'])
+            .getDocuments();
+        await UserData.profileData.sendEmailVerification();
+        await FirebaseAuth.instance.signOut();
+        formState.reset();
+        throw ("Verify");
+      } catch (e) {
+        //_isSignIn = false;
+        print(e);
+        details.clear();
+        currentUser = null;
+        _ackAlert(
+            context,
+            e == "Verify" ? "Verification Required" : "SignUp Failed!",
+            e.toString().contains("Mobile")
+                ? e
+                : e == "Verify"
+                    ? "Verify email and then signIn"
+                    : e.toString().split(',')[1]);
+      }
+    }
+  }
+
+  Future<void> signIn(
+      GlobalKey<FormState> _formKey, BuildContext context) async {
+    details.clear();
+    FormState formState = _formKey.currentState;
+    if (formState.validate()) {
+      Future.delayed(Duration(seconds: 5));
+      formState.save();
+      print(details["CountryCode"] + details["Mobile"]);
+      try {
+        PhoneCodeAutoRetrievalTimeout autoRetrievalTimeout = (String ID) {
+          this.verificationID = ID;
+        };
+        PhoneCodeSent codeSent = (String verID, [int forceResend]) {
+          this.verificationID = verID;
+          _ackAlert(context, "Code Sent", "wait");
+        };
+//        await FirebaseAuth.instance.verifyPhoneNumber(phoneNumber: details["CountryCode"]+details["Mobile"], timeout: Duration(seconds: 120), verificationCompleted: (auth){
+////          await FirebaseAuth.instance.signInWithCredential(auth);
+//        }, verificationFailed: (exp){print(exp.message);}, codeSent: codeSent, codeAutoRetrievalTimeout: autoRetrievalTimeout);
+
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+            email: details['Email'], password: details['Pass']);
+        UserData.profileData = await FirebaseAuth.instance.currentUser();
+        if (!UserData.profileData.isEmailVerified) {
+          await UserData.profileData.sendEmailVerification();
+          await FirebaseAuth.instance.signOut();
+          throw ("Verify");
+        }
+        UserData.detailsData =
+            await userReference.document(UserData.profileData.uid).get();
+        nextPage(context);
+        formState.reset();
+      } catch (e) {
+        details.clear();
+        _ackAlert(
+            context,
+            "Login Failed!",
+            e == "Verify"
+                ? "Verify email and then signIn"
+                : e.toString().split(',')[1]);
+      }
+    }
+  }
+}
---
+++ lib/Utils/theme.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class themeData {
+  ThemeData lightTheme() {
+    return ThemeData(
+      cardTheme: CardTheme(
+        color: Colors.grey[50],
+        margin: EdgeInsets.only(top: 15, left: 15, right: 15),
+      ),
+      scaffoldBackgroundColor: Colors.grey[100],
+      sliderTheme: SliderThemeData(
+        trackHeight: 3,
+        activeTrackColor: Colors.cyan,
+        inactiveTrackColor: Colors.grey[350],
+        disabledActiveTrackColor: Colors.cyanAccent,
+        disabledInactiveTrackColor: Colors.grey[200],
+        activeTickMarkColor: Colors.deepPurple[400],
+        inactiveTickMarkColor: Colors.deepPurple[200],
+        disabledActiveTickMarkColor: Colors.grey,
+        disabledInactiveTickMarkColor: Colors.grey,
+        thumbColor: Colors.cyan,
+        disabledThumbColor: Colors.grey,
+        overlayColor: Color.fromRGBO(0, 188, 212, 0.2),
+        valueIndicatorColor: Colors.cyanAccent,
+        trackShape: RectangularSliderTrackShape(),
+        tickMarkShape: RoundSliderTickMarkShape(tickMarkRadius: 1.2),
+        thumbShape: RoundSliderThumbShape(enabledThumbRadius: 6.0),
+        overlayShape: RoundSliderOverlayShape(overlayRadius: 15.0),
+        valueIndicatorShape: PaddleSliderValueIndicatorShape(),
+        showValueIndicator: ShowValueIndicator.always,
+        valueIndicatorTextStyle: TextStyle(color: Colors.deepPurple),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+          errorStyle: TextStyle(fontWeight: FontWeight.w700),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+                style: BorderStyle.solid, color: Colors.cyan[100], width: 0.5),
+          ),
+          errorBorder: UnderlineInputBorder(
+            borderRadius: BorderRadius.circular(5),
+            borderSide:
+                BorderSide(style: BorderStyle.none, color: Colors.redAccent),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5),
+            borderSide: BorderSide(style: BorderStyle.solid, color: Colors.red),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5),
+            borderSide: BorderSide(
+                style: BorderStyle.solid, color: Colors.cyan, width: 1.0),
+          ),
+          contentPadding: EdgeInsets.all(10.0)),
+      buttonTheme: ButtonThemeData(
+        buttonColor: Colors.cyan,
+      ),
+      primarySwatch: Colors.cyan,
+    );
+  }
+}
